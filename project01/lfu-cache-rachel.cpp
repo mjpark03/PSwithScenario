abdad6169d994681ee70a2e315e034b39626ee1a
mjpark03 <mjpark03@gmail.com>
2017-03-22 23:18:06 +0900

    add datastructures

--- project01/lfu-cache-rachel.cpp
+++ project01/lfu-cache-rachel.cpp
@@ -1,14 +1,32 @@
+#include <unordered_map>
+#include <unordered_set>
+
 class LFUCache {
 public:
-    LFUCache(int capacity) {
-        
+  int capacity;
+  std::unordered_map<int, int> map;
+  std::unordered_map<int, int> countMap;
+  std::unordered_map<int, std::unordered_set<int> > orderMap;
+
+  LFUCache(int capacity) {
+    this -> capacity = capacity;
+  }
+
+  int get(int key) {
+    auto value = map.find(key);
+    if(value == map.end()) {
+      return 0;
     }
-    
-    int get(int key) {
-        
-    }
-    
-    void put(int key, int value) {
-        
-    }
+
+    return 0;
+  }
+
+  void put(int key, int value) {
+    (void)key;
+    (void)value;
+  }
 };
+
+int main() {
+  return 0;
+}
