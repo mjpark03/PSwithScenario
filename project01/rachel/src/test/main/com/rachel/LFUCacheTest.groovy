02e70650270d1851e593f277277a2ab02bdff713
mjpark03 <mjpark03@gmail.com>
2017-05-03 17:58:55 +0900

    Add template for unit test

---
+++ project01/rachel/src/test/main/com/rachel/LFUCacheTest.groovy
@@ -0,0 +1,15 @@
+package main.com.rachel
+
+/**
+ * Created by Rachel on 2017. 5. 3..
+ */
+class LFUCacheTest extends GroovyTestCase {
+    void setUp() {
+        super.setUp()
+
+    }
+
+    void tearDown() {
+
+    }
+}
